225542861b79c8ea699cd355fcffce65b32e6ff1
AnaBraznikova <116310193+AnaBraznikova@users.noreply.github.com>
2023-01-29 17:54:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+
+// let a = prompt();
+// let b = prompt();
+
+// let hour = a * 3600;
+// let day = b * 24 * 3600;
+// console.log(hour + ' секунд в часах');
+// console.log(day + ' секунд в днях' );
+
+
+// num = 3;
+// alert(num)
+
+// a = 10;
+// b = 2;
+
+// console.log(a+b, a-b, a*b, a/b);
+
+// c = 15;
+// d = 2;
+
+// let result = c + d;
+// console.log(result);
+
+// a = 10;
+// b = 2;
+// c = 5;
+
+// console.log(a + b + c);
+
+// a = 17;
+// b = 10;
+// let c = a - b; 
+// d = 7;
+// let result = c + d;
+// console.log(result);
+
+// let str = 'Привет мир!'
+// console.log(str);
+
+// let str1 = 'Привет '
+// let str2 = 'Мир!'
+// console.log(str1 + str2);
+
+// let name = 'Анастасия';
+// console.log('Привет, ' + name + '!');
+
+// let age = '29';
+// console.log('Мне ' + age + ' лет!');
+
+// let a = prompt();
+
+// if (a == 0 ) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// if (a > 0 ) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// if (a < 0 ) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
